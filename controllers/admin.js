684d4a33237c13f15437f9bd95ac20e0997da6e9
ralintuscano <ralintuscano16@student.sfit.ac.in>
2020-09-21 23:38:58 +0530

    seperated controllers

---
+++ controllers/admin.js
@@ -0,0 +1,27 @@
+const Product = require("../models/product");
+
+exports.getAddProduct = (req, res, next) => {
+  res.render("admin/add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect("/");
+};
+
+exports.getAdminProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "Admin Products",
+      path: "/",
+    });
+  });
+};
--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,11 @@ const path = require("path");
 
 const express = require("express");
 
-const productsController = require("../controllers/products");
+const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
-router.get("/", productsController.getProducts);
+router.get("/", shopController.getProducts);
 
 router.get("/products");
 
