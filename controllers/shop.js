e4072c1c5f0a494761a38365dbc91dc0c4f4abd8
ralintuscano <ralintuscano16@student.sfit.ac.in>
2020-09-22 16:38:28 +0530

    added orders view

--- controllers/shop.js
+++ controllers/shop.js
@@ -33,3 +33,10 @@ exports.getCheckout = (req, res, next) => {
     pageTitle: "Checkout",
   });
 };
+
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
+  });
+};
